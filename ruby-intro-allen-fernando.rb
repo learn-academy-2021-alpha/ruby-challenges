6b94f9b8fd91e7d94a0172b7897f5bc21e0d617f
romerofernando <romero_f22@yahoo.com>
2021-03-02 10:54:24 -0800

    first commit

---
+++ ruby-intro-allen-fernando.rb
@@ -0,0 +1,23 @@
+# p 1+2
+# p 4-2
+# p 3 * 4
+# p 12/ 4
+# p 3 ** 3
+# p 9 % 4
+# p 5 / 0
+# p 0 /0
+
+
+# my_favorite_number = 22
+
+# puts my_favorite_number / 2 
+
+# someones_favorite_number = 13
+# p someones_favorite_number
+# someones_favorite_number = 7
+# p someones_favorite_number
+
+# puts my_favorite_number - someones_favorite_number
+
+# my_favorite_number = my_favorite_number * 26 
+#  p my_favorite_number
