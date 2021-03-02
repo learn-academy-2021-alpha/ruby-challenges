fb3aea08f8d1cbf1e3b3ad2077ddf63459af3b5b
LPeterson18 <lpeterson@chorus.net>
2021-03-02 11:06:37 -0800

    Arrays and Strings left

---
+++ intro-deven-lex.rb
@@ -0,0 +1,80 @@
+# Challenges
+# Arithmetic Challenges
+# Remember that floats are numbers with a decimal point whereas integers have no fractional part so in Ruby, 1 and 1.0 are different types of things with slightly different behaviors.
+#
+# Add, subtract, multiply and divide some numbers and floats.
+
+# p 1+3
+# p 5-3
+# p 42-2+2
+# p 25*4
+# p 12/2
+#
+# p 3.0+4.2
+# p 15.5 - 0.5
+# p (6.7*8.8).round(0)
+# p 6.2-7.0
+# p 6.2-7
+# p 6.2/7
+# p (6.2-7).round(0)
+#
+#
+# # Find the remainder of dividing two numbers using the modulo operator (%).
+#
+# p 5%47
+# p 47%5
+# p 48%5
+# # Divide a number by 0.
+#
+# # p 6/0
+#
+# # Divide 0 by 0.
+#
+# # p 0/0
+# p 5.0/0
+# p 0.0/0
+
+
+
+# Variables Challenges
+# Set a variable called my_favorite_number equal to your favorite number
+
+# my_favorite_number = 18
+# p my_favorite_number
+#
+# # Calculate your favorite number divided by 2
+#
+# p my_favorite_number/2
+#
+# # Set another variable called someones_favorite equal to 13
+#
+# someones_favorite = 13
+# p someones_favorite
+#
+# # Change the value of someones_favorite to 7
+#
+# someones_favorite = 7
+# p someones_favorite
+#
+# # Subtract my_favorite_number from someones_favorite
+#
+# p someones_favorite - my_favorite_number
+#
+# # Change the value of my favorite number to be 26 times its current value
+#
+# p my_favorite_number = my_favorite_number * 26
+# p my_favorite_number
+
+# Strings Challenges
+# Create a variable and return it in a sentence string interpolation
+# Create a variable that contains a string and test some of the Ruby string methods
+# .upcase
+# .reverse
+# .include?
+# .capitalize
+# Arrays Challenges
+# Create an array with five of your favorite TV shows
+# Find the length of the array
+# From the full TV show array, extract your top 3 TV shows in a new array
+# Reverse the array
+# Return one element from the array
