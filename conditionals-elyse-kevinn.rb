0e501e7700bfa7c029ae6a62084d16615b7de695
elysemontano <elysemontano@gmail.com>
2021-03-02 12:03:36 -0800

    challenge complete

--- conditionals-elyse-kevinn.rb
+++ conditionals-elyse-kevinn.rb
@@ -7,8 +7,26 @@
 # Story: As a user, I can see a message in the terminal saying if there was a tie.
 
 puts 'User 1 please type "rock", "paper", or "scissors"'
-user_1 = gets
+user_1 = gets.chomp
 
 puts 'User 2 please type "rock", "paper", or "scissors"'
-user_2 = gets
+user_2 = gets.chomp
 
+p user_1
+p user_2
+
+if user_1 == "rock" && user_2 == "paper"
+  puts "user_2 wins!"
+elsif user_1 == "rock" && user_2 == "scissors"
+  puts "user_1 wins!"
+elsif user_1 == "paper" && user_2 == "scissors"
+  puts "user_2 wins!"
+elsif user_1 == "paper" && user_2 == "rock"
+  puts "user_1 wins!"
+elsif user_1 == "scissors" && user_2 == "rock"
+  puts "user_2 wins!"
+elsif user_1 == "scissors" && user_2 == "paper"
+  puts "user_1 wins!"
+else
+  puts "Everyone is a winner!"
+end
