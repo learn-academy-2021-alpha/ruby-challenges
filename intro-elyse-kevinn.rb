07f55ed05e9b3bcca2037e72e8a8fb31f7291281
kevinnarbas <kevinnarbas@gmail.com>
2021-03-02 10:53:19 -0800

    completes arithmetic and variables challenges

---
+++ intro-elyse-kevinn.rb
@@ -0,0 +1,46 @@
+# Arithmetic Challenges
+# Remember that floats are numbers with a decimal point whereas integers have no fractional part so in Ruby, 1 and 1.0 are different types of things with slightly different behaviors.
+
+# Add, subtract, multiply and divide some numbers and floats.
+# p 2 + 2
+# p 3.4 - 2
+# p 5 / 3 
+# p 6 * 4
+# Find the remainder of dividing two numbers using the modulo operator (%).
+# p 6 % 2
+# Divide a number by 0.
+# p 7 / 0
+# Divide 0 by 0.
+# p 0.0 / 0
+
+# Variables Challenges
+# Set a variable called my_favorite_number equal to your favorite number
+my_favorite_number = 7.0
+# Calculate your favorite number divided by 2
+p my_favorite_number / 2
+# Set another variable called someones_favorite equal to 13
+someones_favorite = 13
+p someones_favorite
+# Change the value of someones_favorite to 7
+someones_favorite = 7
+p someones_favorite
+
+# Subtract my_favorite_number from someones_favorite
+p my_favorite_number - someones_favorite
+# Change the value of my favorite number to be 26 times its current value
+p my_favorite_number **= 26
+p my_favorite_number = my_favorite_number ** 26
+p my_favorite_number
+# Strings Challenges
+# Create a variable and return it in a sentence string interpolation
+# Create a variable that contains a string and test some of the Ruby string methods
+# .upcase
+# .reverse
+# .include?
+# .capitalize
+# Arrays Challenges
+# Create an array with five of your favorite TV shows
+# Find the length of the array
+# From the full TV show array, extract your top 3 TV shows in a new array
+# Reverse the array
+# Return one element from the array
