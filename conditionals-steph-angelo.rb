e8fbd7296ad2d3651987746ddd070a21d6744b2c
stephbegle <stephbegle@gmail.com>
2021-03-02 13:15:42 -0800

    sending over to angelo

--- conditionals-steph-angelo.rb
+++ conditionals-steph-angelo.rb
@@ -1,11 +1,10 @@
 puts "type either rock, paper, or scissors"
-user1 = gets
-user2 = gets
+user1 = gets.chomp
+user2 = gets.chomp
 
-# p user1
-# p user2
+p "User 1 is: " + user1
+p "User 2 is: " + user2
 
-# hands = [user1, user2]
 if user1 == "rock" && user2 == "scissors"
   puts "user1 wins"
 elsif user1 == "scissors" && user2 == "rock"
@@ -16,4 +15,11 @@ elsif user1 == "paper" && user2 == "rock"
   puts "user1 wins"
 elsif user1 == "paper" && user2 == "scissors"
   puts "user2 wins"
+elsif user1 == "scissors" && user2 == "paper"
+  puts "user1 wins"
+elsif user1 == "rock" && user2 == "scissors"
+  puts "user1 wins"
+else user1 == "scissors" && user2 == "rock"
+  puts "user2 wins"
 end
+
