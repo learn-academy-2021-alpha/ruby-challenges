5d9d6f8b8abd5983593ecc69ff78015cbd5034b4
algtan <agtan@ymail.com>
2021-03-02 11:36:54 -0800

    completed challenges

--- ruby-intro-allen-fernando.rb
+++ ruby-intro-allen-fernando.rb
@@ -1,3 +1,4 @@
+# Array Challenges
 # p 1+2
 # p 4-2
 # p 3 * 4
@@ -8,6 +9,7 @@
 # p 0 /0
 
 
+# Variable Challenges
 # my_favorite_number = 22
 
 # puts my_favorite_number / 2 
@@ -21,3 +23,43 @@
 
 # my_favorite_number = my_favorite_number * 26 
 #  p my_favorite_number
+
+
+# String Challenges
+# name = "Allen"
+# puts "Hello #{name}, welcome to Ruby"
+
+# test_string = "we are testing Ruby string methods"
+# # p has quotes
+# p test_string.upcase
+# # puts has no quotes
+# puts test_string.upcase
+
+# puts test_string.reverse
+
+# puts test_string.include? "JavaScript"
+
+# puts test_string
+# puts test_string.capitalize
+
+# test_string2 = "this IS A TEST"
+# puts test_string2
+# puts test_string2.capitalize
+
+
+# Array Challenges
+tv_shows = ["SG 1", "The Wire", "Community", "Mandolorian", "WandaVision"]
+
+# p tv_shows.length
+# puts tv_shows.length
+
+favorite_shows = [tv_shows.last, tv_shows[3], tv_shows[2]]
+# print favorite_shows
+p favorite_shows
+# puts favorite_shows
+
+# Reverse of favorite_shows
+# p favorite_shows.reverse
+p favorite_shows.last
+p tv_shows.first
+p tv_shows[2]
