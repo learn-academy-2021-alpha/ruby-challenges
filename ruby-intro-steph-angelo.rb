36e62d45bf94faeee182acd9ef339326d96ac6de
LEARN Academy <>
2021-03-02 10:58:08 -0800

    second driver

---
+++ ruby-intro-steph-angelo.rb
@@ -0,0 +1,35 @@
+p 1 + 3
+
+p 2 * 5
+
+p 4 / 2
+
+p 365 ** 2
+
+p 9.0 / 2
+
+# p 0 / 0
+
+p 0.0 / 0.0
+
+p 0.0 / 0
+
+p 7.0 / 0
+
+p 7 % 2
+
+p 7.0 % 2
+
+my_favorite_number = 69.0
+
+p my_favorite_number / 2
+
+someones_favorite = 13
+
+p someones_favorite - 6
+
+someones_favorite = 7
+
+p someones_favorite - my_favorite_number
+
+p my_favorite_number * 26 
