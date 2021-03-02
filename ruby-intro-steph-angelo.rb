661a302f0fdb9c7928a9373845bd623d42786513
stephbegle <stephbegle@gmail.com>
2021-03-02 11:21:56 -0800

    end of challenges

--- ruby-intro-steph-angelo.rb
+++ ruby-intro-steph-angelo.rb
@@ -1,35 +1,52 @@
-p 1 + 3
+# p 1 + 3
 
-p 2 * 5
+# p 2 * 5
 
-p 4 / 2
+# p 4 / 2
 
-p 365 ** 2
+# p 365 ** 2
 
-p 9.0 / 2
+# p 9.0 / 2
 
-# p 0 / 0
+# # p 0 / 0
 
-p 0.0 / 0.0
+# p 0.0 / 0.0
 
-p 0.0 / 0
+# p 0.0 / 0
 
-p 7.0 / 0
+# p 7.0 / 0
 
-p 7 % 2
+# p 7 % 2
 
-p 7.0 % 2
+# p 7.0 % 2
 
-my_favorite_number = 69.0
+# my_favorite_number = 69.0
 
-p my_favorite_number / 2
+# p my_favorite_number / 2
 
-someones_favorite = 13
+# someones_favorite = 13
 
-p someones_favorite - 6
+# p someones_favorite - 6
 
-someones_favorite = 7
+# someones_favorite = 7
 
-p someones_favorite - my_favorite_number
+# p someones_favorite - my_favorite_number
 
-p my_favorite_number * 26 
+# p my_favorite_number * 26 
+
+word = "this is a sentence"
+
+p "Hello, #{word} incase you couldn't tell."
+
+string = "dragon"
+
+p string.reverse
+p string.upcase
+p string.capitalize
+p string.include? "d"
+
+tv_shows = ["Lost", "Succesion", "Bridgeton", "The 100", "The Office"]
+p tv_shows.length
+
+fav_shows = tv_shows[0,2]
+p fav_shows.last
