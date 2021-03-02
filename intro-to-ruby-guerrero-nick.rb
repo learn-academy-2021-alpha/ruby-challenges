f63d4501149887d624fba2053a558c7cdae9cc0b
nickdsaunders <nickdavidsaunders@gmail.com>
2021-03-02 11:15:09 -0800

    completed variable chaleenges

--- intro-to-ruby-guerrero-nick.rb
+++ intro-to-ruby-guerrero-nick.rb
@@ -2,11 +2,31 @@
 # Find the remainder of dividing two numbers using the modulo operator (%).
 # Divide a number by 0.
 # Divide 0 by 0.
-p 2+2
-p 3-2
-p 6/2
-p 7%2
-p 8/0
-p 0/0
+# p 2+2
+# p 3-2
+# p 6/2
+# p 7%2
+# p 8/0
+# p 0/0
+
+
+# Variables Challenges
+# Set a variable called my_favorite_number equal to your favorite number
+# Calculate your favorite number divided by 2
+# Set another variable called someones_favorite equal to 13
+# Change the value of someones_favorite to 7
+# Subtract my_favorite_number from someones_favorite
+# Change the value of my favorite number to be 26 times its current value
+
+my_favorite_number = 23
+p my_favorite_number / 4
+
+someones_favorite = (13.0 / 2).round
+# p (someones_favorite / 2).round
+p someones_favorite
+p (someones_favorite - my_favorite_number)
+
+p (my_favorite_number * 26)
+
 
 
