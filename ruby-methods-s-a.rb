5bc42fe7327fdb1a824a60376e4e498e3032f3c5
LEARN Academy <>
2021-03-02 14:27:00 -0800

    first challeng complete

---
+++ ruby-methods-s-a.rb
@@ -0,0 +1,33 @@
+# def sum_these_numbers (num1, num2)
+#   num1 + num2
+# end
+# p sum_these_numbers 1, 2
+
+# def is_even num
+#   if num % 2 == 0
+#     puts "is true"
+#   else
+#     puts "is false"
+#   end
+# end
+# p is_even 5
+
+# def below_ten number
+#   if number >= 1 && number <= 10
+#     puts "is valid"
+#   else
+#     puts "is invalid"
+#   end
+# end
+#
+# p below_ten 12
+
+# def palindrome word
+#   if word.reverse == word
+#     puts "THATS A PALINDROME"
+#   else
+#     puts "NOT A PALINDROME"
+#   end
+# end
+# p palindrome "racecar"
+# p palindrome "hello"
