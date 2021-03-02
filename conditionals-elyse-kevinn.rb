ad49f16fb3fe1606ae5603dfea5570f64c5d5ac1
kevinnarbas <kevinnarbas@gmail.com>
2021-03-02 11:48:53 -0800

    adds getting user input

---
+++ conditionals-elyse-kevinn.rb
@@ -0,0 +1,14 @@
+# Story: As user 1, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
+
+# Story: As user 2, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
+
+# Story: As a user, I can see a message in the terminal saying if user 1 or user 2 won the round.
+
+# Story: As a user, I can see a message in the terminal saying if there was a tie.
+
+puts 'User 1 please type "rock", "paper", or "scissors"'
+user_1 = gets
+
+puts 'User 2 please type "rock", "paper", or "scissors"'
+user_2 = gets
+
