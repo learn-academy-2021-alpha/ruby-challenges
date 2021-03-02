0c238c5c40c10806b5e1c6316703b2ace8c1a00d
guerrero <jrheezy.gc@gmail.com>
2021-03-02 10:50:10 -0800

    challenge one complete

---
+++ intro-to-ruby-guerrero-nick.rb
@@ -0,0 +1,12 @@
+# Add, subtract, multiply and divide some numbers and floats.
+# Find the remainder of dividing two numbers using the modulo operator (%).
+# Divide a number by 0.
+# Divide 0 by 0.
+p 2+2
+p 3-2
+p 6/2
+p 7%2
+p 8/0
+p 0/0
+
+
