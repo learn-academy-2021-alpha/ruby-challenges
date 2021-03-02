18a6e9b710416d2426bee3b6080908f529910825
elysemontano <elysemontano@gmail.com>
2021-03-02 11:12:03 -0800

    completed challenges

--- intro-elyse-kevinn.rb
+++ intro-elyse-kevinn.rb
@@ -4,7 +4,7 @@
 # Add, subtract, multiply and divide some numbers and floats.
 # p 2 + 2
 # p 3.4 - 2
-# p 5 / 3 
+# p 5 / 3
 # p 6 * 4
 # Find the remainder of dividing two numbers using the modulo operator (%).
 # p 6 % 2
@@ -31,16 +31,33 @@ p my_favorite_number - someones_favorite
 p my_favorite_number **= 26
 p my_favorite_number = my_favorite_number ** 26
 p my_favorite_number
+
+
 # Strings Challenges
 # Create a variable and return it in a sentence string interpolation
+name = "Kevinn"
+p "My name is #{name}!"
 # Create a variable that contains a string and test some of the Ruby string methods
+p name.upcase
+p name.reverse.downcase
+p name.include? "K"
 # .upcase
 # .reverse
 # .include?
 # .capitalize
+p "hello world. my name is #{name}".capitalize
+
+
 # Arrays Challenges
 # Create an array with five of your favorite TV shows
+tv_shows = ["Heros", "Lost", "Wandavision", "How I Met Your Mother", "The Office"]
+p tv_shows
 # Find the length of the array
+p tv_shows.length
 # From the full TV show array, extract your top 3 TV shows in a new array
+top_three = [tv_shows[0], tv_shows[1], tv_shows[2]]
+p top_three
 # Reverse the array
+p tv_shows.reverse
 # Return one element from the array
+puts tv_shows[4]
