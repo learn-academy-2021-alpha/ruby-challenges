0fe593814870dba5d1f3989fc576025a6ec4972c
LPeterson18 <lpeterson@chorus.net>
2021-03-02 22:57:02 -0800

    completed the rest of the problems

--- blocks-deven-lex.rb
+++ blocks-deven-lex.rb
@@ -54,9 +54,95 @@ end
 p capitalize_words arr2
 
 # Create a method that takes in an array of numbers and returns the largest number in the array.
+
+arr3 = [1, 2, 3, 4, 5, 6]
+
+def find_largest_num array
+  largest_num = array[0]
+  array.map do |number|
+    if number > largest_num
+      largest_num = number
+    end
+  end
+  largest_num
+end
+
+p find_largest_num arr3
+
 # Create a method that takes in an array of numbers and returns the smallest number in the array.
+
+arr4 = [1,2,3,4,5,6,7,8]
+
+def find_smallest_num array
+  smallest_num = array[0]
+  array.map do |number|
+    if number < smallest_num
+      smallest_num = number
+    end
+  end
+  smallest_num
+end
+
+p find_smallest_num arr4
+
 # Create a method that takes in an array of numbers and returns only the odd numbers sorted from least to greatest.
+
+arr5 = [5,1,-4,6,3,10,-11,13,4,5,2,6,13]
+
+def odd_num_sorter array
+  odds_array = array.select do |number|
+    if number.odd?
+      p number
+    end
+  end
+  odds_array.sort
+end
+
+p odd_num_sorter arr5
+
 # Create a method that takes in an array of strings and returns all the strings in reverse casing. All uppercased letters should be returned lowercased and all lowercased letters should be returned uppercased.
+
+arr6 = ["Help", "I", "Need", "Somebody", "help", "i", "Need", "anybody"]
+
+def reverse_string_casing array
+  array.map do |string|
+    string.swapcase
+  end
+end
+
+p reverse_string_casing arr6
+
 # Create a method that takes in an array of words and returns all the words that start with a particular letter. Ex) Given the array ['Apple', 'Avocado', 'Banana', 'Mango'] and the letter 'a' the method should return ['Apple', 'Avocado']. With the same method, given the letter 'm' should return ['Mango'].
+
+arr7 = ["let", "me", "take", "you", "down", "because", "I", "am", "going", "to", "strawberry", "fields", "forever"]
+
+def find_strings_by_letter array, letter
+    array.select do |word|
+      if word.start_with? letter
+        word
+      end
+    end
+end
+
+p find_strings_by_letter arr7, "f"
+p find_strings_by_letter arr7, "t"
+
 # FIZZBUZZ
 # Write a method that prints the numbers from 1 to 100. For multiples of three print Fizz instead of the number, for multiples of five print Buzz instead of the number, for numbers that are multiples of both three and five print FizzBuzz, for all other numbers print the number.
+
+def fizzbuzz_num
+  fizzbuzz_num_range = 1..100
+  fizzbuzz_num_range.each do |value|
+    if value % 5 == 0 && value % 3 == 0
+      p "FizzBuzz"
+    elsif value % 3 == 0
+      p "Fizz"
+    elsif value % 5 == 0
+      p "Buzz"
+    else
+      p value
+    end
+  end
+end
+
+fizzbuzz_num
