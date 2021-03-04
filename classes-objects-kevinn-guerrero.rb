c0c4c56ca2e0a8d450b1db6b7b9f3dc566ef1d77
kevinnarbas <kevinnarbas@gmail.com>
2021-03-04 14:28:01 -0800

    adds logic for color palette

--- classes-objects-kevinn-guerrero.rb
+++ classes-objects-kevinn-guerrero.rb
@@ -29,9 +29,44 @@ end
 homework = Task.new "math homework"
 # p homework.get_title
 # p homework
-p homework.give_description "fractions"
-p homework
- homework.is_done
+# p homework.give_description "fractions"
+# p homework
+#  homework.is_done
 
 
 # Story: As a developer, when I print a Task that is done, its status is shown.
+
+
+# Use attr_accessor for this challenge.
+
+# Story: As a developer, I can create a ColorPalette.
+
+class ColorPalette
+    attr_accessor :palette
+    def initialize 
+        # Story: As a developer, I can initialize my color palette to contain 3 colors.
+        @palette = []
+    end
+
+    # Story: As a developer, I can print the values of each individual color.
+    def print_color 
+        @palette.map do |value|
+            puts "You have chosen #{value}"
+        end   
+        # Story: As a developer, I can print a sentence that tells me all of the colors in my palette.
+    end
+
+
+end
+
+colors = ColorPalette.new 
+colors.palette << 'blue'
+colors.palette << 'red'
+colors.palette << 'yellow'
+p colors
+colors.palette[0] = 'green'
+colors.print_color
+p colors
+
+
+# Story: As a developer, I can change one or more of the colors in my palette.
