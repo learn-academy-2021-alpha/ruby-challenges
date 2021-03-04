1dbabcad44dba177e1a6472d5f297c76c7efac78
romerofernando <romero_f22@yahoo.com>
2021-03-04 10:26:04 -0800

    first commit

---
+++ hashes-fernando-deven.rb
@@ -0,0 +1,26 @@
+# Challenges: Practice with Hashes
+# As a developer, I can create a hash called my_phone using the Ruby method .new.
+my_phone = Hash.new 
+# p my_phone
+
+
+# As a developer, I can add five key:value pairs of app names and their descriptions to my hash.
+my_phone[:instagram] = "pictures of people"
+my_phone[:facebook] = "story feeds and comunication with friends"
+my_phone[:pintrest] = "pictures of things I want to do or see"
+my_phone[:amazon] = "things I want to buy"
+my_phone[:netflix] = "movies and shows I want to waych"
+# p my_phone
+
+# As a developer, I can return a value from my_phone by passing in the name of a key.
+
+
+
+# As a developer, I can update two keys in my_phone.
+
+
+
+
+# As a developer, I can update two values in my_phone.
+# As a developer, I can delete two key:value pairs from my_phone.
+# As a developer, I can use an enumerable method to return information about all of my_phone's applications.
--- methods-allen-fernando.rb
+++ methods-allen-fernando.rb
@@ -1,48 +1,107 @@
 # Create a method called sum_these_numbers which takes two integers as an argument and prints their sum to the screen.
 
-def sum_these_numbers(num1, num2)
-    num1 + num2
-end
+# def sum_these_numbers(num1, num2)
+#     num1 + num2
+# end
 
-# p sum_these_numbers 1, 1
+# # p sum_these_numbers 1, 1
 
-# Create a method called is_even, which takes a single integer, and which then returns true if the number is even, and false otherwise.
+# # Create a method called is_even, which takes a single integer, and which then returns true if the number is even, and false otherwise.
 
-def is_even number
-    if number % 2 == 0
-        true
-    else
-        false
+# def is_even number
+#     if number % 2 == 0
+#         true
+#     else
+#         false
+#     end
+# end
+
+# # p is_even 1
+# # p is_even 4
+
+# # Create a method that takes a number as an argument and prints "Valid" if the number is between 1 and 10 (inclusive) and "Invalid" otherwise.
+
+# def between_one_and_ten number
+#     if number <= 10 && number >= 1
+#         "Valid"
+#     else
+#         "Invalid"
+#     end
+# end
+
+# # p between_one_and_ten 5
+# # p between_one_and_ten 12
+# # p between_one_and_ten 0
+
+# # Create a method that takes in a string and determines if the string is a palindrome.
+
+# def is_palindrome string
+#     if string == string.reverse
+#         "#{string} is a palindrome"
+#     else
+#         "#{string} is NOT a palindrome"
+#     end
+# end
+
+# p is_palindrome "racecar"
+# p is_palindrome "mom"
+# p is_palindrome "father"
+
+# Challenge: Password Checker
+# User Stories
+# You are writing the user registration page for a secure web site. On the registration page, the user has to enter a user ID and a password, which has to adhere to the to following criteria:
+
+# set up the method 2 arguments user_name and password
+#prompt user to input username 
+#prompt user to input password
+
+
+def password_checker user_name, password
+    if user_name == password 
+        "Username and Password can not be the same"
+    elsif password.include?('!')
+        'Password must incude !#$'
+    elsif user_name.length < 6 || password.length < 6 
+        "Username and Password must be at least 6 characters long"
+    elsif user_name.include?('!') || user_name.include?('#') || user_name.include?('$') || user_name.include?(" ")
+        'Username can not incude !#$ or spaces'
     end
 end
 
-# p is_even 1
-# p is_even 4
+puts "Enter Username"
+username = gets.chomp
 
-# Create a method that takes a number as an argument and prints "Valid" if the number is between 1 and 10 (inclusive) and "Invalid" otherwise.
+puts "Enter Password"
+pw = gets.chomp
 
-def between_one_and_ten number
-    if number <= 10 && number >= 1
-        "Valid"
-    else
-        "Invalid"
-    end
-end
+puts password_checker(username, pw)
 
-# p between_one_and_ten 5
-# p between_one_and_ten 12
-# p between_one_and_ten 0
+#user_name 
+# User ID and password cannot be the same.
+# User ID and password must be at least six characters long.
+# User ID cannot contain the following characters: !#$ or spaces
 
-# Create a method that takes in a string and determines if the string is a palindrome.
 
-def is_palindrome string
-    if string == string.reverse
-        "#{string} is a palindrome"
-    else
-        "#{string} is NOT a palindrome"
-    end
-end
+#password
+# Password must contain at least one of: !#$
+# Password cannot be the word password.
+# password must be at least six characters long.
+# password cannot be the same as ID
 
-p is_palindrome "racecar"
-p is_palindrome "mom"
-p is_palindrome "father"
+
+
+
+
+
+
+# As a developer, I can create a method that checks for the following rules for a user ID and password:
+# User ID and password cannot be the same.
+# User ID and password must be at least six characters long.
+# Password must contain at least one of: !#$
+# User ID cannot contain the following characters: !#$ or spaces
+# Password cannot be the word "password".
+# User Stories: Stretch
+# As a user, I can enter my user ID and password into the terminal after being prompted to find out if the they are acceptable.
+# User Stories: Super Stretch
+# As a developer, my method ensures that the user's password must contain at least one number.
+
