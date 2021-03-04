2c3f8609ecb2983c689400d0a1a08688212d9922
stephbegle <stephbegle@gmail.com>
2021-03-04 14:18:01 -0800

    class challenges complete

--- classes-allen-steph.rb
+++ classes-allen-steph.rb
@@ -38,3 +38,32 @@ task1.done
 # Print final "done" status
 p task1.get_done
 p task1
+
+# Color Palette------------------------
+# Story: As a developer, I can create a ColorPalette.
+class ColorPalette
+# Story: As a developer, I can initialize my color palette to contain 3 colors.
+    attr_accessor :color1, :color2, :color3
+# Story: As a developer, I can print the values of each individual color.
+    def initialize (color1, color2, color3)
+        @color1 = color1
+        @color2 = color2
+        @color3 = color3
+    end
+
+    def sentence
+        puts "The colors in your palette are #@color1, #@color2, #@color3"
+    end
+# Story: As a developer, I can print a sentence that tells me all of the colors in my palette.
+# Story: As a developer, I can change one or more of the colors in my palette.
+end
+
+object = ColorPalette.new('red', 'green', 'blue')
+p object.color1
+p object.color2
+p object.color3
+
+object.sentence
+
+object.color1 = 'magenta'
+p object.color1
