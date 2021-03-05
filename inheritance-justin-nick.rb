70f1908bf0ec0fe9ef2379ce66df3db6dfdeeea6
justinhilario <justinhilario@gmail.com>
2021-03-05 11:54:02 -0800

    spaceship operator

--- inheritance-justin-nick.rb
+++ inheritance-justin-nick.rb
@@ -4,6 +4,7 @@
 # Story: As a developer, I can give my Animal an age of 0 upon creation.
 # Story: As a developer, I can age my Animal up one year at a time.
 # Story: As a developer, I can return my Animal's age, as well as if they're alive. Hint: Use attr_accessor as well as an initialize method.
+# Story: As a developer, I can create a Mammal that inherits from Animal.
 
 
 class Animal 
@@ -13,6 +14,7 @@ class Animal
         @alive = alive
         @age = age
     end
+
 
     def age_animal 
         @age += 1
@@ -58,42 +60,104 @@ class Fish < Animal
         end
     end
 end
-
-salmon = Fish.new("alive", 0, "cold_blooded", "Atlantic")
-# p salmon
-# p salmon.cold_blooded
-p salmon.age_animal
-p salmon.age_animal
-p salmon.age_animal
-p salmon.age_animal
-p salmon.old_age
-p salmon.get_info
-
-
-
-
-
-# Story: As a developer, I can create a Mammal that inherits from Animal.
-
 # Story: As a developer, I can initialize all of my Mammals to be warm_blooded.
-
 # Story: As a developer, I can create a Bear that inherits from Mammal.
-
+# Story: As a developer, I can create a Bear that inherits from Mammal.
 # Story: As a developer, I can age my Bear up.
-
 # Story: As a developer, I can see a message that tells me all of my Bear's information.
-
 # Story: As a developer, if my Bear turns 20 years old, I can make it die peacefully after a full and happy life. Hint: You will need a method that changes the status of alive in the initialize method of Animal.
 
+class Mammal < Animal
+
+    attr_accessor :warm_blooded
+
+    def initialize (alive, age, warm_blooded)
+        super(alive, age)
+        @warm_blooded = "warm blooded"
+    end
+
+    def get_info
+        super
+        puts "My mammal is #{warm_blooded}"
+    end
+
+    def age_mammal
+        if @age > 19
+        @alive = "died peacefully"
+        end
+    end
+end
+
+
+salmon = Fish.new("alive", 3, "cold_blooded", "Atlantic")
+# # p salmon
+# # p salmon.cold_blooded
+# p salmon.age_animal
+# p salmon.age_animal
+# p salmon.age_animal
+# p salmon.age_animal
+# p salmon.old_age
+# p salmon.get_info
+
+bear = Mammal.new("alive", 19, "warm blooded")
+# bear.age_animal
+# bear.age_mammal
+# p bear.get_info
+
 # Story: As a developer, I can create a Mammal of my choice.
-
 # Story: As a developer, I can interact with the new Mammal via various methods.
-
 # Story: As a developer, I can see a message that tells me all of my new Mammal's information.
+
+class Otter < Mammal
+    attr_accessor :playful, :tricks
+
+    def initialize (alive, age, warm_blooded, playful, tricks)
+        super(alive, age, warm_blooded)
+        @playful = playful
+        @tricks = tricks
+    end
+
+    def get_info
+        super
+        puts "My otter is #{playful} and does #{tricks}"
+    end
+
+    def age_otter
+        if @age > 19
+        @alive = "died peacefully"
+        end
+    end
+end
+
+river_rat = Otter.new("alive", 15, "warm blooded", "playful", "flips")
+# p river_rat.get_info
+
 
 # Stretch Challenges
 # Story: As a developer, I can keep a collection of two of each Animal. Hint: You'll want to add your Animals into an array.
+    #want collection of two salmons, and two bears
+   
+#    salmon.collection
 
+#     salmon.collection << "salmon"
+#     salmon.collection << "salmon"
+#     bear.collection << "bear"
+#     bear.collection << "bear"
+
+#    animal1.collection = salmon.collection + bear.collection
+
+    collection = []
+
+    collection << salmon.age
+    collection << salmon.age
+    collection << bear.age
+    collection << bear.age
+    collection << river_rat.age
+    collection << river_rat.age
+
+    p collection
+
+    
 # Story: As a developer, I can sort my collection of Animals based on age. Hint: Find out how the spaceship operator can help you with an array.
 
 # Super Stretch Challenge
