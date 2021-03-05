804cfe68633601a070a4171b6097f54c11e8543d
romerofernando <romero_f22@yahoo.com>
2021-03-05 09:36:46 -0800

    last commit

--- ruby-classes-objects-deven-fernando.rb
+++ ruby-classes-objects-deven-fernando.rb
@@ -5,35 +5,72 @@
 # Story: As a developer, I can give a Task a title and retrieve it.
 # Story: As a developer, I can give a Task a description and retrieve it.
 # Story: As a developer, I can mark a Task done.
-class Task
-    attr_accessor :item1, :item2
-  
-    def initialize(item1, item2)
-      @item1 = item1
-      @item2 = item2
-    end
-  end
-  
-  new_task = Task.new("portfolio", "not done")
-  puts new_task.item1
-  puts new_task.item2
-  
-  new_task.item2 = "Done"
-  puts new_task.item1
-  puts new_task.item2
-  
-
-
-
-# Story: As a developer, I can mark a Task done.
-
 # Story: As a developer, when I print a Task that is done, its status is shown.
+
+# class Task
+#     attr_accessor :item1, :item2
+  
+#     def initialize(item1, item2)
+#       @item1 = item1
+#       @item2 = item2
+#     end
+#   end
+  
+#   new_task = Task.new("portfolio", "not done")
+#   puts new_task.item1
+#   puts new_task.item2
+  
+#   new_task.item2 = "Done"
+#   puts new_task.item1
+#   puts new_task.item2
+  
+
+
+
 
 # Challenge: Color Palette
 # Use attr_accessor for this challenge.
-
 # Story: As a developer, I can create a ColorPalette.
 # Story: As a developer, I can initialize my color palette to contain 3 colors.
 # Story: As a developer, I can print the values of each individual color.
 # Story: As a developer, I can print a sentence that tells me all of the colors in my palette.
 # Story: As a developer, I can change one or more of the colors in my palette.
+
+class ColorPalette
+    attr_accessor :color1, :color2, :color3
+
+    def initialize(color1, color2, color3)
+        @color1 = color1
+        @color2 = color2
+        @color3 = color3
+    end
+    def get_color
+        @color1
+        @color2
+        @color3
+    end
+    def show_color
+        puts "The colors in your palette are #@color1, #@color2, #@color3"
+    end
+end
+
+my_color_palette = ColorPalette.new("blue","green","black")
+
+
+
+# my_color_palette.get_color
+
+puts my_color_palette.color1
+puts my_color_palette.color2
+puts my_color_palette.color3
+
+my_color_palette.show_color
+
+my_color_palette.color3 = "pink"
+my_color_palette.show_color
+
+
+
+
+
+
