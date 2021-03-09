a934f1c7b7ad077bff46f7c45783881388827ebb
elysemontano <elysemontano@gmail.com>
2021-03-09 14:18:29 -0800

    order by completed

--- sql-country-elyse-justin.sql
+++ sql-country-elyse-justin.sql
@@ -1,16 +1,16 @@
 
 -- WHERE
 -- What is the population of the US? (HINT: 278357000)
-SELECT name, population 
-FROM country 
+SELECT name, population
+FROM country
 WHERE name = 'United States'
 -- What is the area of the US? (HINT: 9.36352e+06)
 SELECT name, surfacearea
-FROM country 
+FROM country
 WHERE name = 'United States'
 -- Which countries gained their independence before 1963?
 SELECT name, indepyear
-FROM country 
+FROM country
 WHERE indepyear < 1963
 ORDER by indepyear DESC
 
@@ -45,13 +45,61 @@ ORDER BY indepyear DESC
 
 -- ORDER BY
 -- Which fifteen countries have the lowest life expectancy? (HINT: starts with Zambia, ends with Sierra Leonne)
+
+SELECT name, lifeexpectancy
+FROM country
+ORDER BY lifeexpectancy
+LIMIT 15
+
 -- Which fifteen countries have the highest life expectancy? (HINT: starts with Andorra, ends with Spain)
+SELECT name, lifeexpectancy
+FROM country
+WHERE lifeexpectancy IS NOT NULL
+ORDER BY lifeexpectancy DESC
+LIMIT 15
+
 -- Which five countries have the lowest population density (density = population / surfacearea)? (HINT: starts with Greenland)
+SELECT name, population, surfacearea, population / surfacearea
+AS density
+FROM country
+WHERE population > 0
+ORDER BY density
+LIMIT 5
+
+
 -- Which countries have the highest population density?(HINT: starts with Macao)
+SELECT name, population, surfacearea, population / surfacearea
+AS density
+FROM country
+WHERE population > 0
+ORDER BY density DESC
+LIMIT 5
+
 -- Which is the smallest country by area? (HINT: .4)
+SELECT name, surfacearea
+FROM country
+ORDER BY surfacearea
+LIMIT 1
+
 -- Which is the smallest country by population? (HINT: 50)?
+SELECT name, population
+FROM country
+WHERE population > 0
+ORDER BY population
+LIMIT 1
+
 -- Which is the biggest country by area? (HINT: 1.70754e+07)
+SELECT name, surfacearea
+FROM country
+ORDER BY surfacearea DESC
+LIMIT 1
+
 -- Which is the biggest country by population? (HINT: 1277558000)
+SELECT name, population
+FROM country
+ORDER BY population DESC
+LIMIT 1
+
 -- Subqueries: WITH
 -- What are the forms of government for the top ten countries by surface area? (HINT: Number 10 is Kazakstan)
 -- What are the forms of government for the top ten richest nations by gnp?
